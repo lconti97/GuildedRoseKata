c94670adbc2c02eacd3cb66b5a3e9ae1dc557c8c
Lucas Conti <lconti@qualtrax.com>
2017-05-30 11:05:03 -0400

    initial commit

---
+++ GuildedRoseTests/GuildedRoseShopTests.cs
@@ -0,0 +1,16 @@
+using GuildedRose;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GuildedRoseTests
+{
+    [TestClass]
+    public class GuildedRoseShopTests
+    {
+        
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var guildedRose = new GuildedRoseShop();
+        }
+    }
+}
