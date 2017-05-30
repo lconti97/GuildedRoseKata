c1b7670ddf08dd1cd2f6a6ae7d95265219fd2b2d
Lucas Conti <lconti@qualtrax.com>
2017-05-30 13:01:21 -0400

    Test normal item does not decay to negative quality

--- GuildedRoseTests/GuildedRoseShopTests.cs
+++ GuildedRoseTests/GuildedRoseShopTests.cs
@@ -40,12 +40,23 @@ namespace GuildedRoseTests
         [TestMethod]
         public void NormalItemPastSellInDateQualityDecreasesByTwoAfterOneDay()
         {
-            AddItemToShop("Plain Joe's Sword", 10, 0);
+            AddItemToShop("Plain Joe's Dusty Sword", 10, 0);
 
             guildedRoseShop.UpdateQuality();
 
             var updatedItem = guildedRoseShop.Items[0];
             Assert.AreEqual(8, updatedItem.Quality);
+        }
+
+        [TestMethod]
+        public void NormalItemWithZeroQualityRemainsAtZeroAfterOneDay()
+        {
+            AddItemToShop("Plain Joe's Cardboard Sword", 0, 15);
+
+            guildedRoseShop.UpdateQuality();
+
+            var updatedItem = guildedRoseShop.Items[0];
+            Assert.AreEqual(0, updatedItem.Quality);
         }
 
         private void AddItemToShop(String name, Int32 quality, Int32 sellIn)
